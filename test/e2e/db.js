3720dbf3d2c54cc2eaab04ec49e90a023b99c3b4
martypdx <marty@martypdx.com>
2018-04-24 15:37:16 -0700

    ensure auth middleware

--- test/e2e/db.js
+++ test/e2e/db.js
@@ -1,18 +1,20 @@
 require('dotenv').config({ path: './test/e2e/.env' });
 const connect = require('../../lib/util/connect');
 const mongoose = require('mongoose');
-const request = require('./request')
+const request = require('./request');
 
 before(() => connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pirates-test'));    
 after(() => mongoose.connection.close());
 
 module.exports = {
+    
     dropCollection(name) {
         return mongoose.connection.dropCollection(name)
             .catch(err => {
                 if(err.codeName !== 'NamespaceNotFound') throw err;
             });
     },
+
     createUser(data = { email: 'me@me.com', password: 'abc' }) {
         return request
             .post('/api/auth/signup')
