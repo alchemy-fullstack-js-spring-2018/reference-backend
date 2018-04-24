900e3eae6ac80500cc008885eab7ea1c9cc56044
martypdx <marty@martypdx.com>
2018-04-24 14:24:55 -0700

    Working signin and 400 error on email in use

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -21,4 +21,29 @@ describe.only('Auth API', () => {
     it('signup', () => {
         assert.ok(token);
     });
+
+    it('signin', () => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                email: 'me@me.com',
+                password: 'abc'
+            })
+            .then(({ body }) => {
+                assert.ok(body.token);
+            });
+    });
+
+    it('Gives 400 on signup of same email', () => {
+        return request
+            .post('/api/auth/signup')
+            .send({
+                email: 'me@me.com',
+                password: 'abc'
+            })
+            .then(res => {
+                assert.equal(res.status, 400);
+                assert.equal(res.body.error, 'Email exists');
+            });
+    });
 });
