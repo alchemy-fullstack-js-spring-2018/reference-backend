c1489d053751c7e8bc432a4e2547b1e4ea0c9171
martypdx <marty@martypdx.com>
2018-04-24 13:33:05 -0700

    basic test/model setup for User

---
+++ test/unit/User.test.js
@@ -0,0 +1,15 @@
+const { assert } = require('chai');
+const User = require('../../lib/models/User');
+
+describe('User model', () => {
+
+    const user = {
+        email: 'me@me.com'
+    };
+
+    const password = 'abc';
+
+    it('generates hash from password', () => {
+        
+    });
+});
