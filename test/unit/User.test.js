7021d960db67129d33c6883ad1e9b3a702130304
martypdx <marty@martypdx.com>
2018-04-24 13:47:03 -0700

    working user model with hash generation and comparison

--- test/unit/User.test.js
+++ test/unit/User.test.js
@@ -3,13 +3,24 @@ const User = require('../../lib/models/User');
 
 describe('User model', () => {
 
-    const user = {
+    const data = {
         email: 'me@me.com'
     };
-
+    
     const password = 'abc';
 
+    let user = null;
+    beforeEach(() => {
+        user = new User(data);
+        user.generateHash(password);
+    });
+
     it('generates hash from password', () => {
-        
+        assert.ok(user.hash);
+        assert.notEqual(user.hash, password);
+    });
+
+    it('compares password to hash', () => {
+        assert.isOk(user.comparePassword(password));
     });
 });
